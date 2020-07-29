cf6d66130cdc7d5cac6bb25a5ba693e9f559994c
alex <alex.h.z.xu@oocl.com>
2020-07-29 14:22:09 +0800

    [alex xu,spike lu] feature: add should_return_employees_when_get_employees_given_none test

---
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+public class EmployeeServiceTest {
+
+    @Test
+    public void should_return_employees_when_get_employees_given_none() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
+        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
+        given(employeeRepository.getEmployees()).willReturn(employees));
+        //when
+        EmployeeService employeeService = new EmployeeService();
+        List<Employee> employeeList = employeeService.getEmployees();
+        //then
+        assertIterableEquals(employeeList, employees);
+    }
+}
