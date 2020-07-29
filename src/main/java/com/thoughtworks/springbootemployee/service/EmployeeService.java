1be5e0f18f7f6af63d615c3e66cd47238a31e867
alex <alex.h.z.xu@oocl.com>
2020-07-29 14:37:05 +0800

    [alex xu,spike lu] feature: implement to return employees by pages

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
@@ -17,5 +19,9 @@ public class EmployeeService {
 
     public List<Employee> getEmployees() {
         return employeeRepository.getEmployees();
+    }
+
+    public List<Employee> getEmployees(Integer page, Integer pageSize) {
+        return employeeRepository.getEmployees().stream().skip((page - 1) * pageSize).limit(pageSize).collect(Collectors.toList());
     }
 }
