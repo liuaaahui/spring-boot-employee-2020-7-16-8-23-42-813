65a867a6ec8e9a14b04012b015052b4d804fddec
alex <alex.h.z.xu@oocl.com>
2020-07-29 15:49:04 +0800

    [alex xu,spike lu] feature: add test method should_return_company_when_add_company_then_given_company.

--- src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -114,4 +114,31 @@ public class CompanyServiceTest {
 //        then
         assertIterableEquals(companies.get(0).getEmployees(), employees);
     }
+
+
+    @Test
+    public void should_return_company_when_add_company_then_given_company() {
+        //given
+        CompanyRepository employeeRepository = mock(CompanyRepository.class);
+        List<Company> companies = new ArrayList<>();
+        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
+        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
+        companies.add(new Company(1, "oocl"));
+        companies.add(new Company(2, "blibli"));
+        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000.0));
+        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000.0));
+        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000.0));
+        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000.0));
+
+        companies.get(0).setEmployees(employeesOfOOCL);
+        companies.get(1).setEmployees(employeesOfBlibli);
+        given(employeeRepository.getCompanies()).willReturn(companies);
+//        when
+        CompanyService companyService = new CompanyService(employeeRepository);
+        Company company = companyService.addCompany(new Company(3,"huawei"));
+
+//        then
+        assertEquals(companies.stream().filter(addedCompany -> addedCompany.getId().equals(3)).findFirst().orElse(null), company);
+    }
+
 }
