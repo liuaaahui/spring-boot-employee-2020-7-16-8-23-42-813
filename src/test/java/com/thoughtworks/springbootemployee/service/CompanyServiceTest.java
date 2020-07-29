2678262f21503096d9b112a685f1f752620d5e23
alex <alex.h.z.xu@oocl.com>
2020-07-29 18:31:36 +0800

    [alex xu] refactor: use mock to refactor test function

--- src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -1,192 +1,132 @@
-//package com.thoughtworks.springbootemployee.service;
-//
-//import com.thoughtworks.springbootemployee.model.Company;
-//import com.thoughtworks.springbootemployee.model.Employee;
-//import com.thoughtworks.springbootemployee.repository.CompanyRepository;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.mock;
-//
-//public class CompanyServiceTest {
-//    @Test
-//    public void should_return_companies_when_get_companies_then_given_none() {
-//        //given
-//        CompanyRepository employeeRepository = mock(CompanyRepository.class);
-//        List<Company> companies = new ArrayList<>();
-//        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
-//        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
-//        companies.add(new Company(1, "oocl"));
-//        companies.add(new Company(2, "blibli"));
-//        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000.0));
-//        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000.0));
-//
-//        companies.get(0).setEmployees(employeesOfOOCL);
-//        companies.get(1).setEmployees(employeesOfBlibli);
-//        given(employeeRepository.findAll()).willReturn(companies);
-////        when
-//        CompanyService companyService = new CompanyService(employeeRepository);
-//        List<Company> foundCompanies = companyService.getCompanies();
-//
-////        then
-//        assertIterableEquals(companies, foundCompanies);
-//    }
-//
-//    @Test
-//    public void should_return_companies_when_get_companies_then_given_pages() {
-//        //given
-//        CompanyRepository employeeRepository = mock(CompanyRepository.class);
-//        List<Company> companies = new ArrayList<>();
-//        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
-//        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
-//        companies.add(new Company(1, "oocl"));
-//        companies.add(new Company(2, "blibli"));
-//        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000.0));
-//        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000.0));
-//
-//        companies.get(0).setEmployees(employeesOfOOCL);
-//        companies.get(1).setEmployees(employeesOfBlibli);
-//        given(employeeRepository.findAll()).willReturn(companies);
-////        when
-//        CompanyService companyService = new CompanyService(employeeRepository);
-//        List<Company> foundCompanies = companyService.getCompanies(2, 1).getContent();
-//
-////        then
-//        assertIterableEquals(companies.subList(1, 2), foundCompanies);
-//    }
-//
-//
-//    @Test
-//    public void should_return_company_when_get_company_then_given_id() {
-//        //given
-//        CompanyRepository employeeRepository = mock(CompanyRepository.class);
-//        List<Company> companies = new ArrayList<>();
-//        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
-//        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
-//        companies.add(new Company(1, "oocl"));
-//        companies.add(new Company(2, "blibli"));
-//        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000.0));
-//        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000.0));
-//
-//        companies.get(0).setEmployees(employeesOfOOCL);
-//        companies.get(1).setEmployees(employeesOfBlibli);
-//        given(employeeRepository.findAll()).willReturn(companies);
-////        when
-//        CompanyService companyService = new CompanyService(employeeRepository);
-//        Company foundCompany = companyService.getCompany(1);
-//
-////        then
-//        assertEquals(companies.get(0), foundCompany);
-//    }
-//
-//    @Test
-//    public void should_return_employee_when_get_company_employee_then_given_id() {
-//        //given
-//        CompanyRepository employeeRepository = mock(CompanyRepository.class);
-//        List<Company> companies = new ArrayList<>();
-//        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
-//        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
-//        companies.add(new Company(1, "oocl"));
-//        companies.add(new Company(2, "blibli"));
-//        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000.0));
-//        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000.0));
-//
-//        companies.get(0).setEmployees(employeesOfOOCL);
-//        companies.get(1).setEmployees(employeesOfBlibli);
-//        given(employeeRepository.findAll()).willReturn(companies);
-////        when
-//        CompanyService companyService = new CompanyService(employeeRepository);
-//        List<Employee> employees = companyService.getEmployees(1);
-//
-////        then
-//        assertIterableEquals(companies.get(0).getEmployees(), employees);
-//    }
-//
-//
-//    @Test
-//    public void should_return_company_when_add_company_then_given_company() {
-//        //given
-//        CompanyRepository employeeRepository = mock(CompanyRepository.class);
-//        List<Company> companies = new ArrayList<>();
-//        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
-//        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
-//        companies.add(new Company(1, "oocl"));
-//        companies.add(new Company(2, "blibli"));
-//        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000.0));
-//        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000.0));
-//
-//        companies.get(0).setEmployees(employeesOfOOCL);
-//        companies.get(1).setEmployees(employeesOfBlibli);
-//        given(employeeRepository.findAll()).willReturn(companies);
-////        when
-//        CompanyService companyService = new CompanyService(employeeRepository);
-//        Company company = companyService.addCompany(new Company(3, "huawei"));
-//
-////        then
-//        assertEquals(companies.stream().filter(addedCompany -> addedCompany.getId().equals(3)).findFirst().orElse(null), company);
-//    }
-//
-//    @Test
-//    public void should_return_company_when_update_company_then_given_company() {
-//        //given
-//        CompanyRepository employeeRepository = mock(CompanyRepository.class);
-//        List<Company> companies = new ArrayList<>();
-//        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
-//        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
-//        companies.add(new Company(1, "oocl"));
-//        companies.add(new Company(2, "blibli"));
-//        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000.0));
-//        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000.0));
-//
-//        companies.get(0).setEmployees(employeesOfOOCL);
-//        companies.get(1).setEmployees(employeesOfBlibli);
-//        given(employeeRepository.findAll()).willReturn(companies);
-////        when
-//        CompanyService companyService = new CompanyService(employeeRepository);
-//        Company company = companyService.updateCompany(2, new Company(33333, "huawei"));
-//
-////        then
-//        assertEquals(companies.stream().filter(addedCompany -> addedCompany.getId().equals(2)).findFirst().orElse(null), company);
-//    }
-//
-//    @Test
-//    public void should_return_company_when_delete_company_then_given_id() {
-//        //given
-//        CompanyRepository employeeRepository = mock(CompanyRepository.class);
-//        List<Company> companies = new ArrayList<>();
-//        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
-//        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
-//        companies.add(new Company(1, "oocl"));
-//        companies.add(new Company(2, "blibli"));
-//        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000.0));
-//        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000.0));
-//        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000.0));
-//
-//        companies.get(0).setEmployees(employeesOfOOCL);
-//        companies.get(1).setEmployees(employeesOfBlibli);
-//        given(employeeRepository.findAll()).willReturn(companies);
-////        when
-//        CompanyService companyService = new CompanyService(employeeRepository);
-//        Company company = companyService.deleteCompany(1);
-//
-////        then
-//        assertNull(companies.stream().filter(addedCompany -> addedCompany.getId().equals(1)).findFirst().orElse(null));
-//    }
-//}
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+public class CompanyServiceTest {
+    @Test
+    public void should_return_companies_when_get_companies_then_given_none() {
+        //given
+        CompanyRepository employeeRepository = mock(CompanyRepository.class);
+        List<Company> companies = new ArrayList<>();
+        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
+        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
+        companies.add(new Company(1, "oocl"));
+        companies.add(new Company(2, "blibli"));
+        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000.0));
+        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000.0));
+        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000.0));
+        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000.0));
+
+        companies.get(0).setEmployees(employeesOfOOCL);
+        companies.get(1).setEmployees(employeesOfBlibli);
+        given(employeeRepository.findAll()).willReturn(companies);
+//        when
+        CompanyService companyService = new CompanyService(employeeRepository);
+        List<Company> foundCompanies = companyService.getCompanies();
+
+//        then
+        assertIterableEquals(companies, foundCompanies);
+    }
+
+    @Test
+    public void should_return_companies_when_get_companies_then_given_pages() {
+        //given
+        CompanyRepository employeeRepository = mock(CompanyRepository.class);
+        given(employeeRepository.findAll(any(Pageable.class))).willReturn(Page.empty());
+//        when
+        CompanyService companyService = new CompanyService(employeeRepository);
+        Page<Company> foundCompanies = companyService.getCompanies(2, 1);
+
+//        then
+        assertEquals(Page.empty(), foundCompanies);
+    }
+
+
+    @Test
+    public void should_return_company_when_get_company_then_given_id() {
+        //given
+        CompanyRepository employeeRepository = mock(CompanyRepository.class);
+        Company company = new Company(1, "hello");
+        given(employeeRepository.findById(anyInt())).willReturn(Optional.of(company));
+//        when
+        CompanyService companyService = new CompanyService(employeeRepository);
+        Company foundCompany = companyService.getCompany(1);
+//        then
+        assertEquals(company, foundCompany);
+    }
+
+    @Test
+    public void should_return_employee_when_get_company_employee_then_given_id() {
+        //given
+        CompanyRepository employeeRepository = mock(CompanyRepository.class);
+        Company company = mock(Company.class);
+        List<Employee> employees = new ArrayList<>();
+        given(employeeRepository.findById(anyInt())).willReturn(Optional.of(company));
+        given(company.getEmployees()).willReturn(employees);
+//        when
+        CompanyService companyService = new CompanyService(employeeRepository);
+        List<Employee> employeesFound = companyService.getEmployees(1);
+
+//        then
+        assertEquals(employees, employeesFound);
+    }
+
+
+    @Test
+    public void should_return_company_when_add_company_then_given_company() {
+        //given
+        CompanyRepository employeeRepository = mock(CompanyRepository.class);
+        Company company = new Company();
+        given(employeeRepository.save(any(Company.class))).willReturn(company);
+//        when
+        CompanyService companyService = new CompanyService(employeeRepository);
+        Company companyFound = companyService.addCompany(new Company(3, "huawei"));
+
+//        then
+        assertEquals(company, companyFound);
+    }
+
+    @Test
+    public void should_return_company_when_update_company_then_given_company() {
+        //given
+        CompanyRepository employeeRepository = mock(CompanyRepository.class);
+        Company company = new Company();
+        given(employeeRepository.save(any(Company.class))).willReturn(company);
+//        when
+        CompanyService companyService = new CompanyService(employeeRepository);
+        Company companyFound = companyService.updateCompany(2, new Company(33333, "huawei"));
+
+//        then
+        assertEquals(company, companyFound);
+    }
+
+    @Test
+    public void should_return_company_when_delete_company_then_given_id() {
+        //given
+        CompanyRepository employeeRepository = mock(CompanyRepository.class);
+        Company company = new Company();
+        given(employeeRepository.findById(anyInt())).willReturn(Optional.of(company));
+//        when
+        CompanyService companyService = new CompanyService(employeeRepository);
+        Company companyFound = companyService.deleteCompany(1);
+
+//        then
+        assertEquals(company, companyFound);
+    }
+}
--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -1,134 +1,114 @@
-//package com.thoughtworks.springbootemployee.service;
-//
-//import com.thoughtworks.springbootemployee.model.Employee;
-//import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.mock;
-//
-//public class EmployeeServiceTest {
-//
-//    @Test
-//    public void should_return_employees_when_get_employees_given_none() {
-//        //given
-//        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
-//        List<Employee> employees = new ArrayList<>();
-//        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
-//        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
-//        given(employeeRepository.findAll()).willReturn(employees);
-//        //when
-//        EmployeeService employeeService = new EmployeeService(employeeRepository);
-//        List<Employee> employeeList = employeeService.getEmployees();
-//        //then
-//        assertIterableEquals(employeeList, employees);
-//    }
-//
-//    @Test
-//    void should_return_employees_when_get_employees_given_page_pageSize() {
-//        //given
-//        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
-//        List<Employee> employees = new ArrayList<>();
-//        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
-//        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
-//        employees.add(new Employee(3, 18, "hellome2", "male", 1000.0));
-//        employees.add(new Employee(4, 18, "hellome3", "female", 2000.0));
-//        given(employeeRepository.findAll()).willReturn(employees);
-//        //when
-//        EmployeeService employeeService = new EmployeeService(employeeRepository);
-//        List<Employee> employeeList = employeeService.getEmployees(1, 2).getContent();
-//        //then
-//        assertIterableEquals(employeeList, employees.subList(0, 2));
-//    }
-//
-//    @Test
-//    void should_return_employees_when_get_employees_given_gender() {
-//        //given
-//        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
-//        List<Employee> employees = new ArrayList<>();
-//        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
-//        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
-//        employees.add(new Employee(3, 18, "hellome2", "male", 1000.0));
-//        employees.add(new Employee(4, 18, "hellome3", "female", 2000.0));
-//        given(employeeRepository.findAll()).willReturn(employees);
-//        //when
-//        EmployeeService employeeService = new EmployeeService(employeeRepository);
-//        List<Employee> employeeList = employeeService.getEmployees("male");
-//        //then
-//        assertIterableEquals(employeeList, employees.subList(0, 3));
-//    }
-//
-//    @Test
-//    void should_return_employee_when_get_employee_given_id() {
-//        //given
-//        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
-//        List<Employee> employees = new ArrayList<>();
-//        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
-//        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
-//        employees.add(new Employee(3, 18, "hellome2", "male", 1000.0));
-//        employees.add(new Employee(4, 18, "hellome3", "female", 2000.0));
-//        given(employeeRepository.findAll()).willReturn(employees);
-//        //when
-//        EmployeeService employeeService = new EmployeeService(employeeRepository);
-//        Employee employee = employeeService.getEmployee(1);
-//        //then
-//        assertEquals(employees.get(0), employee);
-//    }
-//
-//    @Test
-//    void should_return_employee_when_add_employee_given_employee() {
-//        //given
-//        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
-//        List<Employee> employees = new ArrayList<>();
-//        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
-//        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
-//        employees.add(new Employee(3, 18, "hellome2", "male", 1000.0));
-//        employees.add(new Employee(4, 18, "hellome3", "female", 2000.0));
-//        given(employeeRepository.findAll()).willReturn(employees);
-//        //when
-//        EmployeeService employeeService = new EmployeeService(employeeRepository);
-//        Employee employee = employeeService.addEmployee(new Employee(23, 18, "alex", "male", 121341564.0));
-//        //then
-//        assertEquals(employees.get(employees.size() - 1), employee);
-//    }
-//
-//    @Test
-//    void should_return_employee_when_update_employee_given_employee() {
-//        //given
-//        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
-//        List<Employee> employees = new ArrayList<>();
-//        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
-//        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
-//        employees.add(new Employee(3, 18, "hellome2", "male", 1000.0));
-//        employees.add(new Employee(4, 18, "hellome3", "female", 2000.0));
-//        given(employeeRepository.findAll()).willReturn(employees);
-//        //when
-//        EmployeeService employeeService = new EmployeeService(employeeRepository);
-//        Employee employee = employeeService.updateEmployee(1, new Employee(23, 18, "alex", "female", 1000.0));
-//        //then
-//        assertEquals(employees.get(0).getAge(), employee.getAge());
-//        assertEquals(employees.get(0).getName(), employee.getName());
-//        assertEquals(employees.get(0).getGender(), employee.getGender());
-//    }
-//
-//    @Test
-//    void should_return_employee_when_delete_employee_given_employee_id() {
-//        //given
-//        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
-//        List<Employee> employees = new ArrayList<>();
-//        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
-//        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
-//        employees.add(new Employee(3, 18, "hellome2", "male", 1000.0));
-//        employees.add(new Employee(4, 18, "hellome3", "female", 2000.0));
-//        given(employeeRepository.findAll()).willReturn(employees);
-//        //when
-//        EmployeeService employeeService = new EmployeeService(employeeRepository);
-//        Employee employee = employeeService.delete(1);
-//        //then
-//        assertFalse(employees.stream().anyMatch(staff -> staff.getId().equals(1)));
-//    }
-//}
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+public class EmployeeServiceTest {
+
+    @Test
+    public void should_return_employees_when_get_employees_given_none() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
+        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
+        given(employeeRepository.findAll()).willReturn(employees);
+        //when
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        List<Employee> employeeList = employeeService.getEmployees();
+        //then
+        assertIterableEquals(employeeList, employees);
+    }
+
+    @Test
+    void should_return_employees_when_get_employees_given_page_pageSize() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        given(employeeRepository.findAll(any(Pageable.class))).willReturn(Page.empty());
+        //when
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        Page<Employee> employees = employeeService.getEmployees(1, 2);
+        //then
+        assertNotNull(employees);
+    }
+
+    @Test
+    void should_return_employees_when_get_employees_given_gender() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
+        given(employeeRepository.findAllByGender(anyString())).willReturn(employees);
+        //when
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        List<Employee> employeeList = employeeService.getEmployees("male");
+        //then
+        assertIterableEquals(employeeList, employeeList);
+    }
+
+    @Test
+    void should_return_employee_when_get_employee_given_id() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        Employee employee = new Employee(1, 18, "hello", "male", 1000.0);
+        given(employeeRepository.findById(anyInt())).willReturn(Optional.of(employee));
+        //when
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        Employee employeeFound = employeeService.getEmployee(1);
+        //then
+        assertEquals(employee, employeeFound);
+    }
+
+    @Test
+    void should_return_employee_when_add_employee_given_employee() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        Employee employee = new Employee(23, 18, "alex", "male", 121341564.0);
+        given(employeeRepository.save(any(Employee.class))).willReturn(employee);
+        //when
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        Employee employeeFound = employeeService.addEmployee(employee);
+        //then
+        assertEquals(employee, employeeFound);
+    }
+
+    @Test
+    void should_return_employee_when_update_employee_given_employee() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        Employee employee = new Employee(23, 18, "alex", "female", 1000.0);
+        given(employeeRepository.save(any(Employee.class))).willReturn(employee);
+        //when
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        Employee employeeFound = employeeService.updateEmployee(1, employee);
+        //then
+        assertEquals(employee.getAge(), employee.getAge());
+        assertEquals(employee.getName(), employee.getName());
+        assertEquals(employee.getGender(), employee.getGender());
+    }
+
+    @Test
+    void should_return_employee_when_delete_employee_given_employee_id() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        Employee employee = new Employee(23, 18, "alex", "female", 1000.0);
+        given(employeeRepository.findById(anyInt())).willReturn(Optional.of(employee));
+        //when
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        Employee employeeFound = employeeService.delete(1);
+        //then
+        assertEquals(employee, employeeFound);
+    }
+}
