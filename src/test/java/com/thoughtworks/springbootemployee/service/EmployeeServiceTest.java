6875395dc0627969a71bd7b55cc1202bf87e7816
alex <alex.h.z.xu@oocl.com>
2020-07-29 14:34:26 +0800

    [alex xu,spike lu] feature: add test method should_return_employees_when_get_employees_given_page_pageSize

--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -27,4 +27,22 @@ public class EmployeeServiceTest {
         //then
         assertIterableEquals(employeeList, employees);
     }
+
+    @Test
+    void should_return_employees_when_get_employees_given_page_pageSize() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
+        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
+        employees.add(new Employee(3, 18, "hellome2", "male", 1000.0));
+        employees.add(new Employee(4, 18, "hellome3", "female", 2000.0));
+        given(employeeRepository.getEmployees()).willReturn(employees);
+        //when
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        List<Employee> employeeList = employeeService.getEmployees(1, 2);
+        //then
+        assertIterableEquals(employeeList, employees.subList(0, 2));
+
+    }
 }
