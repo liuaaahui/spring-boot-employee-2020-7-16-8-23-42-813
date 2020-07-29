bd3d6501c6bb177e35451620b34f5edd6890f26b
alex <alex.h.z.xu@oocl.com>
2020-07-29 14:39:12 +0800

    [alex xu,spike lu] feature: add should_return_employees_when_get_employees_given_gender test

--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -43,6 +43,22 @@ public class EmployeeServiceTest {
         List<Employee> employeeList = employeeService.getEmployees(1, 2);
         //then
         assertIterableEquals(employeeList, employees.subList(0, 2));
+    }
 
+    @Test
+    void should_return_employees_when_get_employees_given_gender() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, 18, "hello", "male", 1000.0));
+        employees.add(new Employee(2, 18, "hellome", "male", 1000.0));
+        employees.add(new Employee(3, 18, "hellome2", "male", 1000.0));
+        employees.add(new Employee(4, 18, "hellome3", "female", 2000.0));
+        given(employeeRepository.getEmployees()).willReturn(employees);
+        //when
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        List<Employee> employeeList = employeeService.getEmployees("male");
+        //then
+        assertIterableEquals(employeeList, employees.subList(0, 3));
     }
 }
