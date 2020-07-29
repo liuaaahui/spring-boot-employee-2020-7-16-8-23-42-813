814baebb1f475677eaa7b52f9633e98d45789f72
alex <alex.h.z.xu@oocl.com>
2020-07-29 15:39:13 +0800

    [alex xu,spike lu] feature: add getCompanies method given page and pageSize.

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -5,6 +5,7 @@ import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CompanyService {
     private final CompanyRepository companyRepository;
@@ -15,5 +16,9 @@ public class CompanyService {
     }
     public List<Company> getCompanies() {
         return companyRepository.getCompanies();
+    }
+
+    public List<Company> getCompanies(Integer page, Integer pageSize) {
+        return companyRepository.getCompanies().stream().skip((page-1)*pageSize).limit(pageSize).collect(Collectors.toList());
     }
 }
