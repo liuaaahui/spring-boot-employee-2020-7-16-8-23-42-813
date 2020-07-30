62365284ae6fec264fb3e235a2e52317487cb363
alex <alex.h.z.xu@oocl.com>
2020-07-30 15:45:11 +0800

    [alex xu,penny liu] feature: add integration test case add employee

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeIntegrationTest.java
@@ -5,15 +5,16 @@ import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,13 +27,14 @@ public class EmployeeIntegrationTest {
     private MockMvc mockMvc;
     @Autowired
     private CompanyRepository companyRepository;
-
+    private Integer companyId;
     @BeforeEach
     public void before() {
         Company company = new Company(1, "ali");
         Employee employee = new Employee(1, 18, "alex", "male", 1000.0);
         Company saveCompany = companyRepository.save(company);
         employee.setCompanyId(saveCompany.getId());
+        companyId = saveCompany.getId();
         employeeRepository.save(employee);
     }
 
@@ -52,5 +54,19 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$.code").value(1))
                 .andExpect(jsonPath("$.data[0].name").value("alex"))
                 .andExpect(jsonPath("$.data[0].gender").value("male"));
+    }
+
+    @Test
+    public void should_return_employee_when_post_employee_given_employee() throws Exception {
+        //when then
+        mockMvc.perform(post("/employees")
+                .contentType(MediaType.APPLICATION_JSON).content("{\n" +
+                        "    \"age\":18,\n" +
+                        "    \"name\":\"spy\",\n" +
+                        "    \"gender\":\"female\",\n" +
+                        "    \"companyId\":" + this.companyId + "\n" +
+                        "}")).andExpect(jsonPath("$.code").value(1))
+                .andExpect(jsonPath("$.data.name").value("spy"))
+                .andExpect(jsonPath("$.data.gender").value("female"));
     }
 }
