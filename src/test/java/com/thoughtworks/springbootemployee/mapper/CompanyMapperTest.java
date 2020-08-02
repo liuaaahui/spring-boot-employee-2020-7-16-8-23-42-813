428fe01ba754a24b5c3a0f0034c3480f5537f7cc
penny <925580783@qq.com>
2020-08-02 16:34:07 +0800

    [penny]feature: add should_return_companyResponse_when_mapper_to_response_given_company test

--- src/test/java/com/thoughtworks/springbootemployee/mapper/CompanyMapperTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/mapper/CompanyMapperTest.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.CompanyRequest;
+import com.thoughtworks.springbootemployee.dto.CompanyResponse;
+import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.model.Company;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +25,21 @@ public class CompanyMapperTest {
         assertEquals(companyRequest.getCompanyName(), company.getCompanyName());
         assertEquals(companyRequest.getEmployeesNumber(), company.getEmployeesNumber());
         assertEquals(companyRequest.getEmployees(), company.getEmployees());
+    }
+
+    @Test
+    void should_return_companyResponse_when_mapper_to_response_given_company() {
+        //given
+        CompanyMapper companyMapper = new CompanyMapper();
+        Company company = new Company(1,"alibaba",1000,new ArrayList<>());
+
+        //when
+        CompanyResponse companyResponse = companyMapper.toResponse(company);
+
+        //then
+        assertEquals(company.getId(), companyResponse.getId());
+        assertEquals(company.getCompanyName(), companyResponse.getCompanyName());
+        assertEquals(company.getEmployeesNumber(), companyResponse.getEmployeesNumber());
+        assertEquals(company.getEmployees(), companyResponse.getEmployees());
     }
 }
